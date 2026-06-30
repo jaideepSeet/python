46561de3adff935a741ec8d498b005518c288760
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 19:48:50 +0530

    solve q17

---
+++ week 2/q17.py
@@ -0,0 +1,5 @@
+# What is the difference between the string methods find and index ?
+
+-1 and error
+#answer .find()returns -1 if the substring is not found
+#.index()raises a valu errorif the substring is not found
