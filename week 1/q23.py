6a7be06435279707054335681e200a6c62e0004e
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 17:01:25 +0530

    solve q23

---
+++ week 1/q23.py
@@ -0,0 +1,54 @@
+# Accept a two digit number as input and print the sum of its digits.
+# What about a three digit number?
+
+# input = 256
+# ouput = 2 5 6
+
+n = int(input())
+fir = n // 100
+print(fir)
+
+sec = (n // 10)%10
+print(sec)
+
+thir = n % 10
+print(thir)
+print(fir,sec,thir)
+
+# print(fir+sec+thir)
+#output  enter number 256
+                     #2
+                     #5
+                     #6
+                     #2 5 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
