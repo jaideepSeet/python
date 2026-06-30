e070f05567b59d1a29ed92b161b0a45fccd132e0
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 16:50:02 +0530

    solve q21

---
+++ week 1/q21.py
@@ -0,0 +1,8 @@
+# g(x) is the smallest integer greater than or equal to the real number x. For example,  g(1.2) is  2 and  g(4.8) is 
+# 5. Accept a positive real number 
+# x as input and print 
+# g(x)
+
+
+x =float(input())
+print(int(x)+(x> int(x)))
