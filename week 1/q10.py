029345f3e44c70702bc5874c709d5e161325db62
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 13:18:04 +0530

    solve q10

---
+++ week 1/q10.py
@@ -0,0 +1,9 @@
+## What will be the outcome of the following expression if x and y are two strings?
+
+# len(x) + len(y) == len(x+y)
+
+x = "Jai"
+y = "Sanjana"
+print(len(x) + len(y) == len(x+y))
+
+#answer   true
