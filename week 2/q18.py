121918fbc0e547e6f6f759dc24fd45b10a59803c
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 19:52:33 +0530

    solve q18

---
+++ week 2/q18.py
@@ -0,0 +1,17 @@
+# n=30
+# x=n//2
+# print(x)
+# if x%2==0:
+#     print("Yes")
+# else:
+#     print("No")/
+
+# from gettext import find
+
+
+u="WLWWW"
+print(len(u))
+print(u.count("W"))
+print(u.find("WWW")-1)
+print(u.find("W")-1)
+#answer 5 4 1 -1
