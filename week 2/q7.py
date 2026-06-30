6e48d21f3b6aab36a906505b2c4508048970e0bf
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 17:54:13 +0530

    solve q7

---
+++ week 2/q7.py
@@ -0,0 +1,15 @@
+x = int(input("Enter a number: "))
+
+if x % 10 == 0:
+    print("multiple of 10")
+elif x % 5 == 0:
+    print("multiple of 5 and not a multiple of 10")
+else:
+    print("neither a multiple of 5 nor a multiple of 10")
+
+
+
+#answer  ent a number = 5
+#multiple of 5 and not a multiple of 10
+
+
