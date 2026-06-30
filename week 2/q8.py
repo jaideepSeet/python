c3969fa8815f97b6ea699dfd1175de9f2671498d
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 18:08:57 +0530

    solve q8

---
+++ week 2/q8.py
@@ -0,0 +1,23 @@
+x, y, z = int(input()), int(input()), int(input())
+
+if x == y and y == z:
+    print('equilateral')
+elif x == y and x != z:
+    print('isosceles')
+elif x == z and x != y:
+    print('isosceles')
+else:
+    print('scalene')
+
+x, y, z = int(input()), int(input()), int(input())
+
+if x == y and y == z:
+    print('equilateral')
+elif x == y or y == z or x == z:
+    print('isosceles')
+else:
+    print('scalene')
+  
+  #answer  enter number 5 5 8
+  #issosceles
+  
