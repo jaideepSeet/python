c0f83efa14ea051d05f355f37250567c83685d27
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 17:25:34 +0530

    solve q3

---
+++ week 2/q3.py
@@ -0,0 +1,14 @@
+# good_variable=10
+# a_10=1
+# _a=2
+# 1_a=10
+# is=10
+# had variable =100
+
+good_variable = 10      valid
+a_10 = 1                valid
+_a = 2                  valid
+1_a=10                  Invalid
+ is=10                  invalid
+ had variable =100     invalid
+
