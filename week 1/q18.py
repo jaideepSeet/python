b3e9068d9707741db080cfea6f9ab52e3151bc8e
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 16:32:02 +0530

    solve q18

---
+++ week 1/q18.py
@@ -0,0 +1,9 @@
+# What is the following code snippet trying to do? Try to relate this to the previous question.
+print(float(input())*float(input()) * float(input()))
+
+
+#output  enter 3 decimals  0.1
+                          #2.6
+                          #2.5
+                          #outp  0.65
+                          
