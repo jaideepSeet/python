eaf6b7556e5327f198e8073cb98030c9dce2ba34
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 16:27:32 +0530

    solve q17

---
+++ week 1/q17.py
@@ -0,0 +1,8 @@
+# Accept the length and breadth of a rectangle as input and compute its area.
+
+length=int(input("Enter the length of the rectangle: "))
+breadth=int(input("Enter the breadth of the rectangle: "))
+print("The area of the rectangle is: ",length*breadth)
+#answer   enter length of rect 2
+         #enter breadth of rect 4
+         #output   area of rect is 8
