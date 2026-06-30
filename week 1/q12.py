d9dbf39daf7c6dbc9caedd1be1969ecdcfd9859b
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 16:11:46 +0530

    solve q12

---
+++ week 1/q12.py
@@ -0,0 +1,9 @@
+# Consider the following code snippet.
+
+print(input()+input()+input())
+
+# If the output of the following code snippet is 123 , what is the sequence of inputs entered by the user?
+#answer   1
+         #2
+         #3
+         #output   123
