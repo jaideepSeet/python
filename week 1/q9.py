8ce85a5dbc6ef7d94a0f487c437043b99df0103e
JaideepSeet <njaideepkumar007@gmail.com>
2026-06-30 13:10:19 +0530

    solve q9

---
+++ week 1/q9.py
@@ -0,0 +1,9 @@
+# 
+# If some_string is a string of length n , how many characters does n * some_string have, where n is some positive integer?
+
+some_string = "jai"
+s=len(some_string)
+print(s)
+print(s*some_string)
+print(len(s*some_string))
+
