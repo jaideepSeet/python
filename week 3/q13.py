8f4367068a40c83c8f93fadf162d3cde8e41ac1d
JaideepSeet <njaideepkumar007@gmail.com>
2026-07-01 11:02:29 +0530

    solve q13

---
+++ week 3/q13.py
@@ -0,0 +1,14 @@
+# Write a code to accept a string as input and determine if it is a palindrome or not.
+
+s=input("Enter the number:")
+rev=""
+
+for i in s:    #sant
+    rev = i+rev #tnas
+    
+if s == rev:
+    print("palindrome")
+
+else:
+    print("Not palindrome ")
+ #Answer  enter a number 5        palindrome
