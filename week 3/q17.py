8aa26bb3050da6f0961103a233a5624cd938888d
JaideepSeet <njaideepkumar007@gmail.com>
2026-07-01 12:08:28 +0530

    solve q17

---
+++ week 3/q17.py
@@ -0,0 +1,23 @@
+
+x=int(input())
+pre = None 
+count = 0
+
+while x!=-1:
+    if pre!=x:
+        count+=1
+    pre=x
+    x=int(input())
+print(count)
+       
+   #Answer     5
+                #5
+#9
+#9
+#2
+#-1
+
+
+#3
+        
+            
